c63c94fa0ae45cf37a3e3caf406f092a235103ac
BenICMarkets <bj.viljoen1093@gmail.com>
2026-05-13 00:03:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ status: 'Mission Control is live!' });
-});
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', time: new Date() });
-});
+app.get('/', (req, res) => res.json({ status: 'online' }));
+app.get('/api/health', (req, res) => res.json({ ok: true }));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Mission Control running on port ' + PORT);
-});
-module.exports = app;
+app.listen(PORT, () => console.log('Running on ' + PORT));
